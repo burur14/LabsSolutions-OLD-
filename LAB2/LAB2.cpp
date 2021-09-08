26594be5b9a552a25bbb5b6b548053185c0b3164
Denys <dulovdn@gmail.com>
2021-09-08 16:10:57 +0300

    Added LAB2, renamed LAB1

---
+++ LAB2/LAB2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main() {
+	double x;
+	double y;
+
+	std::cin >> x;
+	std::cin >> y;
+
+	bool result = false;
+	if (y >= x / 2) {
+		if ((x - 2)*(x - 2) + y*y  <= 4){
+			result = true;
+		}
+	}
+
+	std::cout << std::boolalpha << result << std::endl;
+}
