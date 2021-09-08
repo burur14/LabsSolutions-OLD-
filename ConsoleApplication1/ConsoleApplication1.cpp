06f3c8e6bc1528a66965d278e49692688fd0b86f
Denys <dulovdn@gmail.com>
2021-09-08 12:50:24 +0300

    added lab

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,19 @@
 ﻿#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int m;
+	int n;
+
+	std::cin >> m;
+	std::cin >> n;
+
+	double div = (double)m / (double)n;
+
+	int lastIntNum = (int)div % 10;
+	int firstDecimal = (int)(div * 10) % 10;
+
+	std::cout << lastIntNum << std::endl << firstDecimal << std::endl;
+
+	return 0; 
 }
 
