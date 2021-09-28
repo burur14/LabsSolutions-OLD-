fd1f61fc3930fe1226fac6af0e9d1c7f9fc82a22
Denys <dulovdn@gmail.com>
2021-09-28 13:42:03 +0300

    LAB 3 completed

--- LAB3/LAB3.cpp
+++ LAB3/LAB3.cpp
@@ -8,11 +8,12 @@ int main(){
 	double a1 = 1;
 	double a2 = (a1 + (x / a1)) / 2;
 	double tmp;
-	while (abs(a2 - a1 > 0.0001)) {
-		tmp = a2;
+	while (abs(a2 - a1) > 0.0001) {
+		
+		a1 = a2;
 		a2 = (a1 + (x / a1)) / 2;
-		a1 = tmp;
+
 	}
-	::cout << a1 << "  " << a2 << endl;
+	::cout << a2 << endl;
 	return 0;
 }
